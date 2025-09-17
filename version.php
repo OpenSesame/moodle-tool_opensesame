aab499fdb38af5f5e380c441f2c704f3ed0d4e9d
David Castro <davidcastro00@gmail.com>
2025-09-17 11:56:55 -0500

    Bump plugin release version to 1.1.2

--- version.php
+++ version.php
@@ -27,4 +27,4 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2024040201; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2020061500; // Requires this Moodle version. 3.9.
 $plugin->component = 'tool_opensesame'; // Full name of the plugin (used for diagnostics).
-$plugin->release   = '1.0.2';
+$plugin->release   = '1.1.2';
